1155031093cb0be27ea397f69590f60769b252ca
Julius Richardson <juliusrichardson@Juliuss-MBP-2.attlocal.net>
2023-08-02 15:08:25 -0500

    final

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+import { useEffect, useState } from "react";
+
+interface ModalProviderProps {
+    products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({
+    products
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <AuthModal />
+            <UploadModal />
+        </>
+    );
+}
+
+export default ModalProvider;
